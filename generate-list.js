b47c3ccee4d21eef0b2274b56fc9b25d69730fc3
Ariana Ivan <git@arianaivan.com>
2023-05-18 15:28:22 -0700

    Set up script to get all image files into a list with their birthtime

---
+++ generate-list.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
+const directoryPath = __dirname + "/images"; // Replace with the path to your images directory
+
+// Function to retrieve the metadata (name and upload date) of a file
+const getMetadata = (filePath) => {
+  const { birthtime } = fs.statSync(filePath);
+  return {
+    uploadDate: birthtime.toISOString(),
+  };
+};
+
+// Function to get all .jpeg files and their metadata in a directory
+const getJPEGFilesWithMetadata = (directoryPath) => {
+  const files = fs.readdirSync(directoryPath);
+  const jpegFiles = files.filter((file) => path.extname(file) === ".jpeg");
+  const filesWithMetadata = jpegFiles.map((file) => ({
+    file: path.join(directoryPath, file),
+    metadata: getMetadata(path.join(directoryPath, file)),
+  }));
+  return filesWithMetadata;
+};
+
+// Call the function with the directory path and log the result
+const filesWithMetadata = getJPEGFilesWithMetadata(directoryPath);
+console.log(filesWithMetadata);
+
+fs.writeFileSync("./all-images.json", JSON.stringify(filesWithMetadata));
